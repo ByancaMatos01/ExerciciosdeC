70d297ae3f7c3794ce276fd7b4c296cbd177d046
Byanca Matos <122841376+ByancaMatos01@users.noreply.github.com>
2023-03-02 21:42:46 -0300

    Add files via upload

---
+++ Exerc02.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("\nHello World \n");
+    printf ("\nMeu primeiro programa \n");
+    printf("\nQuero ser uma programadora Front END\n");
+
+    return 0;
+}
