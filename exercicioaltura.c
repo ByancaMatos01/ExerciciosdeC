17240c5070576133ec0ae417cc3a3f70d3a4e5d7
Byanca Matos <122841376+ByancaMatos01@users.noreply.github.com>
2023-03-02 22:25:24 -0300

    Add files via upload

---
+++ exercicioaltura.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char sexo;
+    int idade;
+    float peso, altura;
+    printf("Mostrar dados\n ");
+    
+    printf("Digite o seu sexo: ");
+    scanf ("%c",&sexo);
+    printf("Digite a sua idade:  ");
+    scanf ("%d",&idade);
+    printf("Digite o seu  peso :  ");
+    scanf ("%f",&peso);
+     printf("Digite a sua  altura :  ");
+    scanf ("%f",&altura);
+    printf("\n Digite a sua  altura :  \n");
+   printf("sexo: %c\n ", sexo);
+    printf("idade: %d\n ", idade);
+     printf("altura: %f\n ", peso);
+      printf("peso: %f\n ", altura);
+      
+    
+    
+    
+    
+
+    return 0;
+}
