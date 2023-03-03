831c90cfe13028c41c053eba0712bd8ddde386c8
Byanca Matos <122841376+ByancaMatos01@users.noreply.github.com>
2023-03-02 21:58:16 -0300

    Add files via upload

---
+++ usando getchar.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char letra;
+    
+    printf("digite uma letra\n ");
+    letra=getchar();
+    printf("caracter lido: %c\n", letra);
+    
+    return 0;
+}
